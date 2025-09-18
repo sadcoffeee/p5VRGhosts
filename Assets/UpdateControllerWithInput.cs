5a1064d87d05947f4817e4fac6db9707d9cfacc2
sadcoffeee <148781287+sadcoffeee@users.noreply.github.com>
2025-09-18 11:02:01 +0200

    added tutorial scene

---
+++ Assets/UpdateControllerWithInput.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class UpdateControllerWithInput : MonoBehaviour
+{
+    public InputActionReference triggerPulled;
+    float triggerPressed;
+
+    // Update is called once per frame
+    void Update()
+    {
+        triggerPressed = triggerPulled.action.ReadValue<float>();
+        Debug.Log(triggerPressed);
+    }
+}
