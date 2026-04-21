34c59b85b10a14e442219d9323c94b860dfe1ab9
Dot-o-Alex <151622122+ajohan23@users.noreply.github.com>
2026-04-21 20:49:10 +0200

    Added glow to armbands

--- Assets/Scripts/VibrationVisualizer.cs
+++ Assets/Scripts/VibrationVisualizer.cs
@@ -1,10 +1,12 @@
 using UnityEngine;
 using UnityEngine.UI;
+using System;
 
 public class VibrationVisualizer : MonoBehaviour
 {
     public Image[] bars;
     [SerializeField] float maxVibrationValue = 150;
+    [SerializeField] MaterialVisualizer[] materials;
 
     public void SetVibrationValue(float value)
     {
@@ -13,6 +15,39 @@ public class VibrationVisualizer : MonoBehaviour
         foreach(Image bar in bars)
         {
             bar.fillAmount = fillRatio;
+        }
+
+        foreach(MaterialVisualizer material in materials)
+        {
+            material.SetIntensityValue(fillRatio);
+        }
+    }
+}
+
+[Serializable]
+public class MaterialVisualizer
+{
+    public MeshRenderer meshRenderer;
+    public int materialIndex;
+    public Vector2 intensityRange;
+    public string propertyName = "_Glow_intensity";
+
+    public void SetIntensityValue(float value)
+    {
+        float intensity = Mathf.Lerp(intensityRange.x, intensityRange.y, value);
+        Material mat = meshRenderer.materials[materialIndex];
+        if (mat == null)
+        { 
+            Debug.LogWarning("Material Not found"); 
+            return; 
+        }
+        if (mat.HasProperty(propertyName))
+        {
+            mat.SetFloat(propertyName, intensity);
+        }
+        else
+        {
+            Debug.Log($"Property {propertyName} not found on material {mat.name}");
         }
     }
 }
